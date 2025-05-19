f6acdd44f8ec251923ece36be1a43461f35bf4bb
azmenezi <azmenezi@gmail.com>
2025-05-19 19:37:34 +0300

    Task 2

--- app/src/main/java/com/example/trueorfalse/MainActivity.kt
+++ app/src/main/java/com/example/trueorfalse/MainActivity.kt
@@ -1,59 +1,113 @@
 package com.example.trueorfalse
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.trueorfalse.ui.theme.TrueOrFalseTheme
 
+val questions = listOf(
+    mapOf("question" to "The Earth revolves around the Sun.", "answer" to true),
+    mapOf("question" to "Bats are blind.",                    "answer" to false),
+    mapOf("question" to "Java was released before Kotlin.",   "answer" to true)
+)
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             TrueOrFalseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                  Column(
-                      modifier = Modifier.
-                      fillMaxSize().
-                      padding(innerPadding).
-                      padding(16.dp),
-                      verticalArrangement = Arrangement.SpaceBetween,
-                      horizontalAlignment = Alignment.CenterHorizontally
+                var answered by rememberSaveable { mutableStateOf(false) }
+                var chosenCorrect by rememberSaveable { mutableStateOf(false) }
+                var qIndex by rememberSaveable { mutableStateOf(0) }
+                var currentQ by rememberSaveable { mutableStateOf<Map<String, Any>>(questions[0]) }
+                var score by rememberSaveable { mutableStateOf(0) }
 
-                  ) {
-                      Question()
-                      ResultCircle(modifier = Modifier, isCorrect = true)
-                      Row (modifier=Modifier.fillMaxWidth().padding(16.dp), horizontalArrangement = Arrangement.SpaceEvenly){
-                          CustomButton(buttonText = "False!", buttonType = false)
-                          CustomButton(buttonText = "True!", buttonType = true)
-                      }
-                  }
+                Scaffold(Modifier.fillMaxSize()) { inner ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(inner)
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.SpaceBetween,
+                        horizontalAlignment  = Alignment.CenterHorizontally
+                    ) {
+                        Question(text = currentQ["question"] as String)
+
+                        if (answered) ResultCircle(isCorrect = chosenCorrect,score=score)
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            if(!answered){
+                            CustomButton(
+                                text           = "False!",
+                                buttonType     = false,
+                                answered       = answered,
+                                onAnswer       = { isTrue ->
+                                    answered      = true
+                                    chosenCorrect = (isTrue == currentQ["answer"])
+                                    if(chosenCorrect)score++
+                                },
+                                onNext = {
+                                    qIndex   = (qIndex + 1) % questions.size
+                                    answered = false
+                                    currentQ = questions[qIndex]
+                                }
+                            )
+
+                                CustomButton(
+                                    text           = "True!",
+                                    buttonType     = true,
+                                    answered       = answered,
+                                    onAnswer       = { isTrue ->
+                                        answered      = true
+                                        chosenCorrect = (isTrue == currentQ["answer"])
+                                        if(chosenCorrect)score++
+                                    },
+                                    onNext = {
+
+                                    }
+                                )
+                            }else{
+                                CustomButton(
+                                    text           = "Next!",
+                                    buttonType     = true,
+                                    answered       = answered,
+                                    onAnswer       = { isTrue ->
+                                        answered      = true
+                                        chosenCorrect = (isTrue == currentQ["answer"])
+                                    },
+                                    onNext = {
+                                        qIndex   = (qIndex + 1) % questions.size
+                                        answered = false
+                                        currentQ = questions[qIndex]
+                                    }
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -61,45 +115,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Question(modifier: Modifier=Modifier){
-    Text(   text = "question?",
-        fontSize = 30.sp,
-        fontWeight = FontWeight.ExtraBold
-
-    )
-}
+fun Question(text: String) =
+    Text(text, fontSize = 30.sp, fontWeight = FontWeight.ExtraBold)
 
 @Composable
-fun ResultCircle(modifier: Modifier=Modifier,isCorrect: Boolean,){
+fun ResultCircle(isCorrect: Boolean,score:Int) {
+    val color = if (isCorrect) Color.Green else Color.Red
     Box(
-       modifier =  modifier.clip(CircleShape).height(300.dp).width(300.dp).background(Color.Green),
-
-
-    )
+        modifier = Modifier
+            .clip(CircleShape)
+            .size(300.dp)
+            .background(color),
+    ){
+        Column(modifier = Modifier.fillMaxSize(),horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Text(text = score.toString(), fontSize = 50.sp)
+        }
+    }
 }
 
 @Composable
-fun CustomButton(modifier: Modifier=Modifier,buttonText: String = "Button Text", buttonType: Boolean){
+fun CustomButton(
+    text: String,
+    buttonType: Boolean,
+    answered: Boolean,
+    onAnswer: (Boolean) -> Unit,
+    onNext: () -> Unit
+) {
     Button(
-        onClick = {},
-        modifier = Modifier,
-        colors = ButtonColors(
-            contentColor =  Color.White ,
-            containerColor = if (buttonType) Color.Blue else Color.Red,
-            disabledContentColor = Color.White,
-            disabledContainerColor = Color.Gray
-        ),
+        onClick = {
+            if (answered) onNext() else onAnswer(buttonType)
+            Log.d("Click", "answered=$answered buttonType=$buttonType")
+        },
         shape = RoundedCornerShape(12.dp),
-        contentPadding = PaddingValues(16.dp)
-
-    ){
-        Text(text = buttonText, fontWeight = FontWeight.Bold, fontSize = 24.sp)
+        contentPadding = PaddingValues(16.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor         = if (buttonType) Color.Blue else Color.Red,
+            contentColor           = Color.White,
+            disabledContainerColor = Color.Gray,
+            disabledContentColor   = Color.White
+        )
+    ) {
+        Text(text, fontWeight = FontWeight.Bold, fontSize = 24.sp)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TrueOrFalseTheme {
-    }
-}
+fun Preview() = TrueOrFalseTheme { Question("Preview") }
